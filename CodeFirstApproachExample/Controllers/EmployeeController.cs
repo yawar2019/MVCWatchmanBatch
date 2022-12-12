3cd8dcadca7eb18b839bfae32bdfc264422e53c4
Azam <Azam@AZAM-PC>
2022-12-12 22:40:22 +0530

    committed

---
+++ CodeFirstApproachExample/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using CodeFirstApproachExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeFirstApproachExample.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        EmployeeContext db = new EmployeeContext();
+        public ActionResult Index()
+        {
+            return View(db.EmployeeModels.ToList());
+        }
+    }
+}
